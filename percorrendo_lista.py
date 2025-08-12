090cf37ac08ed4aa5b1f5f1a746d33416c1b96b8
Antônio Gabriel <antoniothecode@gmail.com>
2025-08-12 20:01:18 -0300

    solucao_concluida_percorrendo_lista.py

---
+++ percorrendo_lista.py
@@ -0,0 +1,5 @@
+def correndo(lista):
+  for item in lista:
+    print(item)
+natureza = ["rio", "floresta", "leão", "montanha", "oceano", "estrela", "vento", "cachoeira", "lua", "girassol"]
+correndo(natureza)
