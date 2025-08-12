635e8333cdce09d34660fb1a479741fe665ba146
Antônio Gabriel <antoniothecode@gmail.com>
2025-08-12 20:26:07 -0300

    solução concluída do  somando_valores_da_lista.py

---
+++ somando_valores_da_lista.py
@@ -0,0 +1,6 @@
+def soma_valores(lista):
+  numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+  total = sum(numeros)
+  print(f"Está é a sua lista {numeros}")
+  print(f"Está é a soma da sua lista {total}")
+soma_valores(numeros)
