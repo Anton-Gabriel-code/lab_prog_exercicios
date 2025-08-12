8fbfc472615e2165019d21e833310f1ac1aa2920
Antônio Gabriel <antoniothecode@gmail.com>
2025-08-12 20:04:45 -0300

    solucao_conccluida_do_encontrando_o_maior_valor.py

---
+++ encontrando_o_maior_valor.py
@@ -0,0 +1,5 @@
+def maior_que(lista):
+  maior = max(lista)
+  print(f"Este é o maior número da sua lista {maior}.")
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+maior_que(numeros)
