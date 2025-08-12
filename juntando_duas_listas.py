a1ad8cc901b95e6275fe0c3b85d40110d7462ebd
Antônio Gabriel <antoniothecode@gmail.com>
2025-08-12 20:32:03 -0300

    solução concluída do juntando_duas_listas.py

---
+++ juntando_duas_listas.py
@@ -0,0 +1,7 @@
+def together(lista1, lista2):
+  lista_juntos = lista1 + lista2
+  return lista_juntos
+natureza = ["rio", "floresta", "leão", "montanha", "oceano", "estrela", "vento", "cachoeira", "lua", "girassol"]
+tecnologia = ["algoritmo", "API", "blockchain", "código", "software", "nuvem", "hardware", "interface", "pixel", "servidor"]
+lista_arroz = together(natureza, tecnologia)
+print(lista_arroz)
